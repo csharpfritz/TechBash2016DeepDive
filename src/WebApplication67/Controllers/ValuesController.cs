ca5b772e6101c9a6adca2b47c1df4721acb3cb32
Jeffrey T. Fritz <jeffreytfritz@gmail.com>
2016-09-29 14:56:20 -0400

    First Commit

---
+++ src/WebApplication67/Controllers/ValuesController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication67.Controllers
+{
+
+    public class Product
+  {
+
+    public int ID { get; set; }
+
+    public string Name { get; set; }
+
+
+  }
+
+
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return new Product[] {
+              new Product { ID=1, Name="Hat" },
+              new Product { ID=2, Name="Stuff" }
+
+            };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
